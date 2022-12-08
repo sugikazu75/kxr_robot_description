eb5f06d1ab04e28283dbbcdd44ce184ba5402739
sugihara <sugihara@jsk.imi.i.u-tokyo.ac.jp>
2022-12-08 23:04:13 +0900

    add shell script for generate models

---
+++ model/khrsugihara/make.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+
+rosrun collada_urdf urdf_to_collada khrsugihara.urdf khrsugihara.dae
+rosrun euscollada collada2eus khrsugihara.dae khrsugihara.yaml khrsugihara.l
+
+rosrun collada_urdf urdf_to_collada khrsugihara-tri-tilt.urdf khrsugihara-tri-tilt.dae
+rosrun euscollada collada2eus khrsugihara-tri-tilt.dae khrsugihara.yaml khrsugihara-tri-tilt.l
+
+rosrun collada_urdf urdf_to_collada khrsugihara-birotor.urdf khrsugihara-birotor.dae
+rosrun euscollada collada2eus khrsugihara-birotor.dae khrsugihara.yaml khrsugihara-birotor.l

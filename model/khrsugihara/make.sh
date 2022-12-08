17c072d90457c2d90d410953b500497eb8f71b4c
sugihara <sugihara@jsk.imi.i.u-tokyo.ac.jp>
2022-12-08 23:14:18 +0900

    add build directory

--- model/khrsugihara/make.sh
+++ model/khrsugihara/make.sh
@@ -1,5 +1,8 @@
 #! /bin/bash
 
+rm build/*.l
+rm build/*.dae
+
 rosrun collada_urdf urdf_to_collada khrsugihara.urdf khrsugihara.dae
 rosrun euscollada collada2eus khrsugihara.dae khrsugihara.yaml khrsugihara.l
 
@@ -8,3 +11,6 @@ rosrun euscollada collada2eus khrsugihara-tri-tilt.dae khrsugihara.yaml khrsugih
 
 rosrun collada_urdf urdf_to_collada khrsugihara-birotor.urdf khrsugihara-birotor.dae
 rosrun euscollada collada2eus khrsugihara-birotor.dae khrsugihara.yaml khrsugihara-birotor.l
+
+mv *.dae build
+mv *.l build
